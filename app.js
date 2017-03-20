9bec969d187af08722883ea19afcdc757fb2e774
soukwinder <sanjosinghmaddan@hotmail.com>
2017-03-20 12:00:05 +0100

    amk

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 /**
- * Created by Swendley on 17-2-2017.
+ * Created by Soukwinder on 17-2-2017.
  */
 var express = require('express')
 
 var app = express();
 var bodyParser = require('body-parser')
 var MongoClient = require('mongodb').MongoClient
+var moment = require('moment');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -14,7 +15,33 @@ app.use(bodyParser.json());
 var port = process.env.PORT || 3000;
 var router = express.Router();
 
+var min = 12;
+var max = 30;
+var temp;
+var currentTime;
+var moment;
+
+var randomData = setInterval(createData, 3000);
+
+function createData(){
+    temp = Math.floor(Math.random() * (max - min + 1)) + min;
+    currentTime = new Date()
+    //moment =  moment().format('MMMM Do YYYY, h:mm:ss a');
+
+}
+
+function createdData() {
+    return '{"DateTime":'+currentTime+', "Temperature":'+temp+'}'
+}
+
+function random() {
+    console.log(createdData())
+    return createdData()
+}
+
+
 router.get('/', function (req, res) {
+
     MongoClient.connect('mongodb://localhost:27017/testdatabase', function (err, db) {
         if (err) throw err
 
@@ -27,8 +54,16 @@ router.get('/', function (req, res) {
     })
 })
 
-app.use('/api', router);
 
-app.listen(port, function () {
-    console.log('Example app listening on port 3000!')
-})
+    router.get('/temp', function (req, res) {
+            res.send(random())
+    });
+
+
+    app.use('/api', router);
+
+
+    app.listen(port, function () {
+        console.log('Example app listening on port 3000!')
+    })
+
