a992f96e23b61cd7498aa0ecf0a8f5a8e9aa7d0f
swendleys <sprott95@gmail.com>
2017-04-09 16:04:41 +0200

    controle streamdata uitval gefixt

--- app.js
+++ app.js
@@ -4,30 +4,38 @@ var app = express();
 var bodyParser = require('body-parser')
 var MongoClient = require('mongodb').MongoClient
 var moment = require('moment');
-
+var data;
+var c
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
-
+var lastUpdatedData;
 var port = process.env.PORT || 3000;
 var router = express.Router();
+var timer = setInterval(checkIfDataUpdated, 5000);
 
 router.get('/', function (req, res) {
     res.send("Please go to localhost:3000/api/temp")
 
 })
-    router.get('/temp', function (req, res) {
+    router.get('/temp', function (req, res, next) {
         MongoClient.connect('mongodb://localhost:27017/ictlab', function (err, db) {
-            if (err) throw err
+            if (err)  { return next(err); }
 
             db.collection('temp').find().toArray(function (err, result) {
                 if (err) throw err
 
-                res.send(result)
-
+                if (JSON.stringify(c)==JSON.stringify(result)){
+                    console.log("statement klopt")
+                    return next(res.status(403).send("error"))
+                }
+                data = result
+               return res.send(result)
             })
+
         })
     });
+
 
 
     app.use('/api', router);
@@ -36,4 +44,8 @@ router.get('/', function (req, res) {
     app.listen(port, function () {
         console.log('Example app listening on port '+port+'!')
     })
+
+    function checkIfDataUpdated() {
+            c = data
+    }
 
