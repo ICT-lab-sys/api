136dbe01b7d041fcae03c472c5fbc70d91a9d1fc
swendleys <sprott95@gmail.com>
2017-04-19 12:36:36 +0200

    controle streamdata uitval gefixt & verificatie

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var express = require('express')
-
 var app = express();
 var bodyParser = require('body-parser')
 var MongoClient = require('mongodb').MongoClient
 var moment = require('moment');
 var data;
-var c
-app.use(bodyParser.urlencoded({ extended: true }));
+var c;
+var countedInterval;
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(bodyParser.json());
-
+var lastUpdatedData;
 var port = process.env.PORT || 3000;
 var router = express.Router();
 var timer = setInterval(checkIfDataUpdated, 5000);
@@ -18,39 +18,59 @@ router.get('/', function (req, res) {
     res.send("Please go to localhost:3000/api/temp")
 
 })
-    router.get('/temp', function (req, res, next) {
-        MongoClient.connect('mongodb://localhost:27017/ictlab', function (err, db) {
-            if (err)  { return next(err); }
+router.get('/temp', function (req, res, next) {
+    MongoClient.connect('mongodb://localhost:27017/ictlab', function (err, db) {
+        if (err) {
+            return next(err);
+        }
 
-            db.collection('temp').find().toArray(function (err, result) {
-                if (err) throw err
+        db.collection('temp').find().toArray(function (err, result) {
+            if (err) throw err
 
-                if (JSON.stringify(c)==JSON.stringify(result)){
-                    console.log("statement klopt")
-                     next(res.status(500).send(result))
-                    if(res.statusCode = 500){
-                        console.log("server down")
+
+            if (JSON.stringify(c) == JSON.stringify(result)) {
+                countedInterval++;
+                console.log(countedInterval)
+
+                //                  Dit is voor Intermittent failure
+                if (countedInterval == 3) {
+                    next(res.status(500).send(result))
+                    if (res.statusCode = 500) {
+                        console.log("Intermittent failure")
                     }
                     return;
                 }
+                //                  Dit is voor Server Down
+                if (countedInterval == 30) {
+                    next(res.status(500).send(result));
+                    if (res.statusCode = 500) {
+                        console.log("Server is down")
+                    }
+                    return;
+                }
+                next(res.status(304).send(result));
+                return;
+             }
 
+            if (JSON.stringify(c) != JSON.stringify(result) && res.statusCode == 200) {
+                countedInterval = 0;
+                console.log(countedInterval);
                 data = result
-               return res.send(result)
-            })
-
+                return res.send(result)
+            }
         })
-    });
-
-
-
-    app.use('/api', router);
-
-
-    app.listen(port, function () {
-        console.log('Example app listening on port '+port+'!')
     })
+});
 
-    function checkIfDataUpdated() {
-            c = data
-    }
+
+app.use('/api', router);
+
+
+app.listen(port, function () {
+    console.log('Example app listening on port ' + port + '!')
+})
+
+function checkIfDataUpdated() {
+    c = data
+}
 
