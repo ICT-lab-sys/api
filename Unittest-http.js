ecaed6aad34b6227e2cfc6ece8ce2a5a58419622
y0u-s <youssef.lakdime@gmail.com>
2017-05-10 12:40:07 +0200

    Unittest HTTP Response

---
+++ Unittest-http.js
@@ -0,0 +1,18 @@
+
+var request = require('request');
+var assert = require('assert'),
+    http = require('http');
+var expect = require('chai').expect,
+    supertest = require('supertest'),
+    api = supertest('http://localhost:3000/api/temp');
+
+
+describe('Unit Test 1: Check if API is live', function () {
+    it('should return 200, OK', function (done) {
+        http.get('http://localhost:3000/api/temp', function (res) {
+            assert.equal(200, res.statusCode);
+            done();
+        });
+    });
+});
+
